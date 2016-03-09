6f66632b6144941870bde05083bf75210f2e15bf
老朱去哪儿 <laozhuqunaer0629@laozhuqunaerdeMacBook-Pro.local>
2016-03-09 11:30:01 +0800

    qqqq

---
+++ ssss/zxj123/zxj123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zxj123
+//
+//  Created by 老朱去哪儿 on 16/3/9.
+//  Copyright © 2016年 老朱去哪儿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ssss/zxj123/zxj123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zxj123
+//
+//  Created by 老朱去哪儿 on 16/3/9.
+//  Copyright © 2016年 老朱去哪儿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
